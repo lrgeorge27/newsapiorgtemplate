b9202fe4d2bb93ccbf3dc9c7bb4bb8876a32df4b
lrgeorge27 <lrgeorge27@gmail.com>
2017-11-27 22:37:01 +0000

    dynamic drop down works, added event handler

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
-/* global $*/ /* global notation can be use outside of jQuery, if var are in another js file.*/
+/* global $ APIKEY*/ /* global notation can be use outside of jQuery, if var are in another js file.*/
 $(document).ready(function() {
     $.ajax({
         method: "GET",
         url: "https://newsapi.org/v2/sources?",
-        data: { category: "business", country: "us", language: "en", apiKey: "81a20d8907024bec97102b55c17d9742" },
+        data: { category: "technology", country: "us", language: "en", apiKey: APIKEY },
         success: function(data) {
             if (data.status == "ok") {
                 console.log(data);
                 for (var i = 0; i < data.sources.length; i++) { //did not do sources[] because we are running through entire array
                     var source = document.createElement("OPTION");
+                    source.setAttribute("value", data.sources[i].id)
                     source.innerHTML = data.sources[i].name;
                     document.getElementById("selection").appendChild(source)
+
+
+
                 }
             }
         }
+
     })
     // .done(function(data) { //method chain
     //     console.log(data);
     //     console.log(data.status)
     // });
+    $("#source").submit(function(event) {
+        document.getElementById("display").innerHTML = data.sources[i].name
+        event.preventDefault();
+        alert(document.getElementById("selection").value)
+    });
+
 });
